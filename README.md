f27375c34ca5756c8bfd86d424d7faba7574baf9
anikolaj <anikolaj@umich.edu>
2026-08-19 00:51:11 -0400

    - initialized project structured

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bittensor Allocation
+
+A CLI that generates market-weighted allocations across Bittensor subnets.
---
+++ main.py
@@ -0,0 +1,9 @@
+import argparse
+
+
+def main() -> None:
+    print("Running bittensor-allocation!")
+
+
+if __name__ == "__main__":
+    main()
